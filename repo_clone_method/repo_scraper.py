10c0ba9b86e11ede0ef1a84429f42c4f238c022a
Sam <113455539+SamShrubo@users.noreply.github.com>
2024-10-20 00:01:41 -0500

    updated scraper for higher granularity

--- repo_clone_method/repo_scraper.py
+++ repo_clone_method/repo_scraper.py
@@ -33,7 +33,8 @@ headers = {
 # Define the query parameters
 # licenses  = ['mit', 'apache-2.0', 'gpl-3.0', 'bsd-3-clause', 'mpl-2.0']
 licenses = ['mit']
-languages = ['Verilog', 'SystemVerilog']
+# languages = ['Verilog', 'SystemVerilog']
+languages = ['Verilog']
 filenames =['.v', '.sv', '.vh', '.vlg', '.verilog']
 
 
@@ -93,101 +94,109 @@ def runScraper():
     # Loop through each year from 2008 to the current year
     current_year = datetime.now().year
     for year in range(2008, current_year+1):
-        start_date = f"{year}-01-01"
-        end_date = f"{year}-12-31"
-        print(f"\n--- Searching for repos created in {year} ---\n")
-        for license_ in licenses:
-            for language in languages:
-                params = {
-                    'q': f'language:{language} license:{license_} created:{start_date}..{end_date}',
-                    'sort': 'stars',                      # Sort by stars (popularity)
-                    'order': 'desc',                      # Descending order
-                    'per_page': 100,                       # Number of results per page
-                    'page': 1                             # Page number
-                }
+        # start_date = f"{year}-01-01"
+        # end_date = f"{year}-12-31"
+        quarters = [
+            ("Q1", f"{year}-01-01", f"{year}-03-31"),
+            ("Q2", f"{year}-04-01", f"{year}-06-30"),
+            ("Q3", f"{year}-07-01", f"{year}-09-30"),
+            ("Q4", f"{year}-10-01", f"{year}-12-31"),
+        ]
+        for quarter_name, start_date, end_date in quarters:
+            print(f"\n--- Searching for repos created in {year} {quarter_name} ---\n")
+            for license_ in licenses:
+                for language in languages:
+                    params = {
+                        'q': f'language:{language} license:{license_} created:{start_date}..{end_date}',
+                        'sort': 'stars',                      # Sort by stars (popularity)
+                        'order': 'desc',                      # Descending order
+                        'per_page': 100,                       # Number of results per page
+                        'page': 1                             # Page number
+                    }
 
-                # while loop for any pagination
-                while True:
-                    # Send the request to GitHub's search API
-                    response = requests.get(SEARCH_API_URL, headers=headers, params=params)
+                    # while loop for any pagination
+                    while True:
+                        # Send the request to GitHub's search API
+                        response = requests.get(SEARCH_API_URL, headers=headers, params=params)
 
-                    # Check if the response is successful
-                    print(f'Querying: {params['q']}, Page: {params["page"]}...', end='')
-                    while response.status_code == 403:
-                        print("403 Error: Potential rate limit cap")
-                        if check_rate_limit():
-                            # after wait period attempt the request again
-                            response = requests.get(SEARCH_API_URL, headers=headers, params=params)
-                        else:
-                            print(f"Unknown Error Received: skipping query: {response.status_code} - {response.text}")
-                            break
-                    
-                    if response.status_code == 200:
-                        data = response.json()
-                        repositories = data['items']
-                        print(f" Success, returned {len(repositories)} repos")
-                        logfd = open(log_filename, 'a')
-                        logfd.write(f"Querying: {params['q']}, Page: {params["page"]}... Success, returned {len(repositories)} repos\n")
-                        logfd.close()
-                        # if no files returned then page is empty
-                        if not repositories:
-                            print("No more files found on this page. Moving to the next query.")
-                            params['page'] += 1
-                            time.sleep(7)
-                            break
-                        
-                        # Print repository information
-                        outfd = open(info_filename, 'a', encoding='utf-8', errors="ignore")
-                        for repo in repositories:
-                            # fill dataframe
-                            newStructure = {'URL' : [repo['html_url']]}
-                            if not op.isfile(url_filename):
-                                df = pd.DataFrame(newStructure)
+                        # Check if the response is successful
+                        print(f"Querying: {params['q']}, Page: {params['page']}...", end='')
+                        while response.status_code == 403:
+                            print("403 Error: Potential rate limit cap")
+                            if check_rate_limit():
+                                # after wait period attempt the request again
+                                response = requests.get(SEARCH_API_URL, headers=headers, params=params)
                             else:
-                                df = pd.read_csv(url_filename)
-                                df = pd.concat([df, pd.DataFrame(newStructure)], ignore_index=True)
-                                # duplicate checking - prevent adding duplicates
-                                dupes = df.duplicated()
-                                if not (df[dupes].empty):
-                                    continue
-                            df.to_csv(url_filename, index=False)
+                                print(f"Unknown Error Received: skipping query: {response.status_code} - {response.text}")
+                                break
+                        
+                        if response.status_code == 200:
+                            data = response.json()
+                            repositories = data['items']
+                            print(f" Success, returned {len(repositories)} repos")
+                            logfd = open(log_filename, 'a')
+                            logfd.write(f"Querying: {params['q']}, Page: {params['page']}... Success, returned {len(repositories)} repos\n")
+                            logfd.close()
+                            # if no files returned then page is empty
+                            if not repositories:
+                                print("No more files found on this page. Moving to the next query.")
+                                params['page'] += 1
+                                time.sleep(7)
+                                break
+                            
+                            # Print repository information
+                            outfd = open(info_filename, 'a', encoding='utf-8', errors="ignore")
+                            for repo in repositories:
+                                # fill dataframe
+                                newStructure = {'URL' : [repo['html_url']]}
+                                if not op.isfile(url_filename):
+                                    df = pd.DataFrame(newStructure)
+                                else:
+                                    df = pd.read_csv(url_filename)
+                                    df = pd.concat([df, pd.DataFrame(newStructure)], ignore_index=True)
+                                    # duplicate checking - prevent adding duplicates
+                                    dupes = df.duplicated()
+                                    if not (df[dupes].empty):
+                                        continue
+                                df.to_csv(url_filename, index=False)
 
 
-                            # write to doc file
-                            outfd.write(f"Name: {repo['name']}\n")
-                            outfd.write(f"Owner: {repo['owner']['login']}\n")
-                            outfd.write(f"Size (KB): {repo['size']}\n")
-                            outfd.write(f"URL: {repo['html_url']}\n")
-                            outfd.write(f"Description: {repo['description']}\n")
-                            count += 1
-                            total_repo_size += repo['size']
-                            outfd.write(f"Repo-Count: {count}\n")
-                            outfd.write(f"Current-Bank-Size-(KB): {total_repo_size}\n\n")
-                            print(f"Current Bank Size: {total_repo_size} KB")
+                                # write to doc file
+                                outfd.write(f"Name: {repo['name']}\n")
+                                outfd.write(f"Owner: {repo['owner']['login']}\n")
+                                outfd.write(f"Size (KB): {repo['size']}\n")
+                                outfd.write(f"URL: {repo['html_url']}\n")
+                                outfd.write(f"Description: {repo['description']}\n")
+                                count += 1
+                                total_repo_size += repo['size']
+                                outfd.write(f"Repo-Count: {count}\n")
+                                outfd.write(f"Current-Bank-Size-(KB): {total_repo_size}\n\n")
+                                print(f"Current Bank Size: {total_repo_size} KB")
+                                print(f"Time Elapsed: {time.time() - start_time} Seconds")
 
-                            # clone into clone dir
-                            # add count into the clone name to prevent failures due to same repo names
-                            clone_path = os.path.join(clone_dir, f'{count}-{repo['name']}')
-                            try:
-                                subprocess.run(['git', 'clone', repo['html_url'], clone_path], check=True)
-                                print(f"Successfully cloned {repo['name']} to {clone_path}\n")
-                                successful += 1
-                                outfd.write(f"Cloned Successfully Count: {successful}\n\n")
-                                successful_repo_size += repo['size']
-                            #check for failed cloning
-                            except subprocess.CalledProcessError as e:
-                                print(f"Failed to clone {repo['name']}: {e}\n")
-                                outfd.write(f"Cloned Successfully Count: {successful}\n\n")
-                                logfd = open(log_filename, 'a')
-                                logfd.write(f"Failed to clone {repo['name']}: {e}\n")
-                                logfd.close()
-                        # print(count)
-                        # Go to the next page of results
-                        params['page'] += 1
-                    else:
-                        print(f"Failed to fetch repositories: {response.status_code} - {response.text}")
-                        break
-                time.sleep(2)
+                                # clone into clone dir
+                                # add count into the clone name to prevent failures due to same repo names
+                                clone_path = os.path.join(clone_dir, f"{count}-{repo['name']}")
+                                try:
+                                    subprocess.run(['git', 'clone', repo['html_url'], clone_path], check=True)
+                                    print(f"Successfully cloned {repo['name']} to {clone_path}\n")
+                                    successful += 1
+                                    outfd.write(f"Cloned Successfully Count: {successful}\n\n")
+                                    successful_repo_size += repo['size']
+                                #check for failed cloning
+                                except subprocess.CalledProcessError as e:
+                                    print(f"Failed to clone {repo['name']}: {e}\n")
+                                    outfd.write(f"Cloned Successfully Count: {successful}\n\n")
+                                    logfd = open(log_filename, 'a')
+                                    logfd.write(f"Failed to clone {repo['name']}: {e}\n")
+                                    logfd.close()
+                            # print(count)
+                            # Go to the next page of results
+                            params['page'] += 1
+                        else:
+                            print(f"Failed to fetch repositories: {response.status_code} - {response.text}")
+                            break
+                    time.sleep(2)
 
     outfd.close()
     end_time = time.time()
